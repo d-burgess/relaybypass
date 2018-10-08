b1c7754a53b1c31d40a90a0662aaf83899267480
d-burgess <43961478+d-burgess@users.noreply.github.com>
2018-10-08 15:15:40 +0100

    Update relayonpress.c

--- relayonpress.c
+++ relayonpress.c
@@ -1,8 +1,11 @@
 /* 
  * File:   relayonpress.c
- * Author: Benoit
+ * Author: Benoit M from Coda Effects
+ * Created on 5 February 2017
  *
- * Created on 5/02/2017
+ * Modified by Dan Burgess
+ * 5 October 2018 
+ * For latching relay and startup option
  */
 
 #include <stdio.h>
@@ -11,125 +14,150 @@
 #include <xc.h>
 #include "header.h"
 
-// Initialization
-void main(void) {
-   ANSEL = 0; // no analog GPIOs
-   CMCON = 0x07; // comparator off
-   ADCON0 = 0; // Analog to Digital and Digital to Analog convertors off
-   TRISIO0 = 0; // output LED
-   TRISIO1 = 1; // input footswtich
-   TRISIO2 = 0; // output TGP222A
-   TRISIO5 = 0; // output activation relay
-   TRISIO4 = 0; // output ground relay
-   TRISIO3 = 1; // input temporary switch
+// Functions
+void flashLed ( uint16_t delayInMs ) { // 65535 max
+    
+    GP0 = 1; // LED on
+    __delay_ms( delayInMs );
+    GP0 = 0; // LED off
+    __delay_ms( delayInMs );
 
-   GPIO = 0; // set outputs as low level (0V)
+}
+
+uint8_t pedalOn ( uint8_t delayInMs ) { // 255 max
+    
+    GP2 = 1; // photoFET on
+    __delay_ms( delayInMs );
+    GP0 = 1; // LED on
+    
+    // Send relay pulse
+    GP5 = 1; // negative relay output on
+    GP4 = 0; // positive relay output off
+    __delay_ms( 5 );
+    GP5 = 0; // negative relay output off
+    GP4 = 0; // positive relay output off   
+    
+    __delay_ms( delayInMs );
+    GP2 = 0; // photoFET off
+    
+    return 1;
+}
+
+uint8_t pedalOff ( uint8_t delayInMs ) { // 255 max
+    
+    GP2 = 1; // photoFET on
+    __delay_ms( delayInMs );
+    GP0 = 0; // LED off
+    
+    // Send relay pulse
+    GP5 = 0; // negative relay output off
+    GP4 = 1; // positive relay output on
+    __delay_ms( 5 );
+    GP5 = 0; // negative relay output off
+    GP4 = 0; // positive relay output off  
+    
+    __delay_ms( delayInMs );
+    GP2 = 0; // photoFET off
+    
+    return 0;
+}
+
+uint8_t changePedalState ( uint8_t state ) {
+    
+    if( state == 1 ) {
+        pedalOn( 20 );
+        return 1;
+    } else {
+        pedalOff( 20 );
+        return 0;
+    }
+
+}
+
+// Initialisation
+void main( void ) {
+    
+    // Initialise PIC
+    ANSEL = 0; // no analog GPIOs
+    CMCON = 0x07; // comparator off
+    ADCON0 = 0; // Analog to Digital and Digital to Analog convertors off
+    
+    // Configure IO
+    // PIC pin 8 GND
+    TRISIO0 = 0; // IO 0 LED output - PIC pin 7
+    TRISIO1 = 1; // IO 1 footswitch input - PIC pin 6
+    TRISIO2 = 0; // IO 2 photoFET output - PIC pin 5
+    TRISIO3 = 1; // IO 3 startup option switch input - PIC pin 4
+    TRISIO4 = 0; // IO 4 positive relay output - PIC pin 3
+    TRISIO5 = 0; // IO 5 negative relay output - PIC pin 2
+    // PIC pin 1 +5V
+
+    GPIO = 0; // set outputs as low level (0V)
    
-   // Variables definition
-   uint8_t state; // on-off state of the pedal (1 = on, 0 = off)
-   state = 0;
-   
-   uint8_t changestate; // to change status of the pedal
-   changestate = 0;
-   
-   uint8_t temporary; // define the mode of the pedal : classic of temporary activation
-   temporary = 0;
-   
-   uint8_t press_switch; // variable to detect if the switch is pressed
-   press_switch = 0;
-   
-   // Main loop
-   while(1) {
-       if(GP3 == 0) { // By default: normal mode
-          temporary = 0;
-      }
-      else { // If the "hold" mode is activated through the switch
-          temporary = 1;
-      }
-       
-       // Normal mode
-       if(temporary == 0) { 
-           if(GP1 == 0) { // footswitch pressed
-               __delay_ms(15); // debouncing
-               if(GP1 == 0) { 
-                  press_switch = press_switch + 1; // switch is on
-                  if(press_switch>10) {
-                      press_switch = 10; // max value for press_switch
-                  } 
-               }
-           }
-           if(press_switch == 1) { // switch is pressed : lets turn the pedal on or off
-               changestate = 1;
-               press_switch = 2; // avoid bugs if press_switch stays at 1
-           }
-           if(GP1 == 1) {
-               __delay_ms(15); // debouncing
-               if(GP1 == 1) { 
-                  press_switch = 0; 
-               }
-           }
-       }
-       
-       // Temporary mode
-       if(temporary == 1) {
-           if(GP1 == 0) { // if switch is pressed : temporary activation
-               __delay_ms(15); // debouncing
-               if(GP1 == 0) { 
-                  press_switch = press_switch + 1; // switch is on
-                  if(press_switch>10) {
-                      press_switch = 10; // max value for press_switch
-                  } 
-               }
-           }
-           if(GP1 == 1) { // if switch is not pressed, turn it off
-               __delay_ms(15); // debouncing
-               if(GP1 == 1) { 
-                   state = 0;
-                   press_switch = 0;
-               }
-           }
-           if(press_switch == 1) {
-               changestate = 1; // if switch is pressed, turn the pedal on
-               press_switch = 2; // avoids bug
-           }
-       }
-       
-       // Changing state of the pedal
-       if(changestate == 1) {
-           if(state == 0) { // change to on
-               GP2 = 1; // photoFET on
-               __delay_ms(20);
-               GP0 = 1; // LED on
-               GP5 = 1; // relay on
-               GP4 = 0;
-               __delay_ms(20);
-               GP2 = 0; // photoFET off
-               state = 1;
-           }
-          else { // change to off
-               GP2 = 1;
-               __delay_ms(20);
-               GP0 = 0; // LED off
-               GP5 = 0; // relay off
-               GP4 = 0;
-               __delay_ms(20);
-               GP2 = 0;
-               state = 0;
-           }
-          __delay_ms(10);
-          changestate=0; // reset changestate
-       }
-       
-       // To let the pedal in the good state (on or off)
-       if (state == 1) { // effect on
-            GP0 = 1; // LED on
-            GP5 = 1; // relay on
-            GP4 = 0; 
-       }
-        else { // effect off
-            GP0 = 0; // LED off
-            GP5 = 0; // relay off
-            GP4 = 0; 
-       }
-   }
+    // Variables definition
+    uint8_t state; // on-off state of the pedal (1 = on, 0 = off)
+    uint8_t startupOption; // startup option switch state (1 = on, 0 = off)
+    uint8_t switchStatus; // variable to detect if the switch is pressed
+    uint8_t statusChangeAllowed; // Variable to allow switch operation to change pedal state
+
+    // Check startup option switch
+    // If pin 4 is low, startup option is on (switch is closed)
+    if ( GP3 == 0 ) { // startup option is on
+        startupOption = 1;
+    } else { // startup option is off
+        startupOption = 0;
+    }
+    
+    // Initialise variables
+    switchStatus = 0;
+    statusChangeAllowed = 0;
+    
+    // Flash LED, twice if startup option off, three times if startup option on
+    // One flash
+    flashLed( 500 );
+    // Two flashes
+    flashLed( 500 );
+    // Third flash
+    if ( startupOption == 1 ) {
+        flashLed( 500 );
+        state = changePedalState( 1 ); // turn pedal on (startup option)
+    } else {
+        state = changePedalState( 0 ); // turn pedal off
+    }
+    
+    // Main loop
+    while( 1 ) {
+        
+        // Check switch is down
+        if( GP1 == 0 ) { // footswitch down
+            __delay_ms( 15 ); // debouncing
+            if( GP1 == 0 ) { // switch still down
+                if( switchStatus != 1 ) {
+                    switchStatus = 1;
+                }
+            }
+        }
+        
+        // Check switch is up
+        if( GP1 == 1 ) { // footswitch up
+            __delay_ms( 15 ); // debouncing
+            if( GP1 == 1 ) { // footswitch still up
+                if( switchStatus != 0 ) {
+                    switchStatus = 0;
+                }
+                if( statusChangeAllowed != 1 ) {
+                    statusChangeAllowed = 1;
+                }
+            }
+        }
+        
+        // Change the pedal state if switch has been pressed and status change is allowed
+        if( ( switchStatus == 1 ) && ( statusChangeAllowed == 1 ) ) { // switch is down: turn the pedal on or off
+            state = changePedalState( !( state ) );
+            __delay_ms( 10 );
+            statusChangeAllowed = 0; // no more status changes allowed until switch is up again
+        }
+        
+    }
+    
 }
